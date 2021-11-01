ad52bf544689ee43b6caf1deecf71c85376e0236
Vic Shostak <vic@shostak.dev>
2021-11-01 20:07:34 +0300

    Del last_name field from register form

--- src/components/forms/UserRegisterForm.vue
+++ src/components/forms/UserRegisterForm.vue
@@ -6,25 +6,14 @@
     </div>
     <div class="mt-6 mb-3">
       <Input
-        v-model="form.user_metadata.first_name"
-        :id="'first_name'"
-        :label="'First name'"
+        v-model="form.user_metadata.full_name"
+        :id="'full_name'"
+        :label="'Full name'"
         :inputType="'text'"
-        :placeholder="'Enter your first name'"
+        :placeholder="'Enter your first and last name'"
         :helpText="''"
         :tabIndex="1"
         :isRequired="true"
-      />
-    </div>
-    <div class="my-3">
-      <Input
-        v-model="form.user_metadata.last_name"
-        :id="'last_name'"
-        :label="'Last name'"
-        :inputType="'text'"
-        :placeholder="'Enter your last name'"
-        :helpText="''"
-        :tabIndex="2"
       />
     </div>
     <div class="my-3">
@@ -35,7 +24,7 @@
         :inputType="'email'"
         :placeholder="'Enter your email address'"
         :helpText="'Only a valid email, we\'ll send a confirmation email to it.'"
-        :tabIndex="3"
+        :tabIndex="2"
         :isRequired="true"
       />
     </div>
@@ -47,7 +36,7 @@
         :inputType="'password'"
         :placeholder="'Enter your secret password'"
         :helpText="'At least 8 characters, letters in different registers, numbers.'"
-        :tabIndex="4"
+        :tabIndex="3"
         :isRequired="true"
       />
     </div>
@@ -57,7 +46,7 @@
           <Switch
             v-model="form.user_metadata.marketing_email_subscription"
             :class="form.user_metadata.marketing_email_subscription ? 'bg-main' : 'bg-secondary'"
-            tabindex="5"
+            tabindex="4"
             class="
               relative
               inline-flex
@@ -80,7 +69,7 @@
       </SwitchGroup>
     </div>
     <div class="my-6">
-      <Button @click="register" :action="'success'" :tabIndex="6" class="w-full">And now register me!</Button>
+      <Button @click="register" :action="'success'" :tabIndex="5" class="w-full">And now register me!</Button>
     </div>
     <div class="mt-8 text-center text-sm">
       Already have an account? <router-link :to="{ name: 'login' }">Login</router-link>!
@@ -88,7 +77,7 @@
   </div>
   <div class="w-64" v-else>
     <h1 class="mb-8 text-center">Oh, wait...</h1>
-    <div class="text-center"><strong>Friend,</strong> you're already registered!</div>
+    <div class="text-center"><strong>Friend</strong>, you're already registered!</div>
     <div class="mt-8">
       <Button @click="() => $router.replace({ name: 'account' })" :tabIndex="1" class="w-full"> Go to account </Button>
     </div>
@@ -123,8 +112,7 @@ export default defineComponent({
       email: '',
       password: '',
       user_metadata: {
-        first_name: '',
-        last_name: '',
+        full_name: '',
         marketing_email_subscription: true,
       },
     })
